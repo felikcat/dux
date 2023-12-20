d940e5ad938546b7463816f3b75610e4a28cc43d
felikcat <29991266+felikcat@users.noreply.github.com>
2023-12-20 03:19:14 -0800

    Remove base-devel and install Docker by default

--- src/02-post_chroot_root.sh
+++ src/02-post_chroot_root.sh
@@ -124,7 +124,7 @@ if [[ ! -a "/tmp/UpdateDB.empty" ]]; then
 fi
 
 # Default packages, regardless of options selected.
-PKGS+=(grub os-prober plymouth opendoas
+PKGS+=(grub os-prober plymouth opendoas docker docker-compose
 irqbalance power-profiles-daemon thermald dbus-broker gamemode lib32-gamemode iptables-nft
 audit apparmor python-notify2 python-psutil bubblewrap-suid
 chrony dnsmasq openresolv libnewt pigz pbzip2 strace usbutils linux-firmware gnome-keyring avahi nss-mdns
@@ -170,7 +170,7 @@ pacman -Syuu --quiet --noconfirm --ask=4 --needed "${PKGS[@]}"
 SERVICES+=(fstrim.timer btrfs-scrub@-.timer
 irqbalance.service dbus-broker.service power-profiles-daemon.service thermald.service
 avahi-daemon.service chronyd.service
-rfkill-unblock@all apparmor.service)
+rfkill-unblock@all apparmor.service docker.service)
 
 systemctl enable "${SERVICES[@]}"
 
