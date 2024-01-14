f7b9cf93b20edf7a6a3189075dd0f22e4d16c5c1
felikcat <29991266+felikcat@users.noreply.github.com>
2024-01-13 23:21:56 -0800

    Update 02-post_chroot_root.sh

--- src/02-post_chroot_root.sh
+++ src/02-post_chroot_root.sh
@@ -124,7 +124,7 @@ if [[ ! -a "/tmp/UpdateDB.empty" ]]; then
 fi
 
 # Default packages, regardless of options selected.
-PKGS+=(grub os-prober plymouth
+PKGS+=(efibootmgr grub os-prober plymouth
 irqbalance power-profiles-daemon thermald dbus-broker gamemode lib32-gamemode iptables-nft
 audit apparmor python-notify2 python-psutil bubblewrap-suid
 chrony dnsmasq openresolv libnewt pigz pbzip2 strace usbutils linux-firmware gnome-keyring avahi nss-mdns
